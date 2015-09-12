cabc948095d782b1b8f9f3201e94eb3ec5ab1523
Romain ASNAR <romain.asnar@gmail.com>
2015-09-12 19:29:44 +0200

    Add outlets to storyboard. related to #1.

---
+++ AniMee/Classes/Modules/Anime/List/Week/Cell/AnimeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AnimeCollectionViewCell.swift
+//  AniMee
+//
+//  Created by Issouf M'sa Benaly on 9/12/15.
+//  Copyright (c) 2015 Mobilette. All rights reserved.
+//
+
+import UIKit
+
+class AnimeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var animeImage: UIImageView!
+    @IBOutlet weak var animeLabel: UILabel!
+}
