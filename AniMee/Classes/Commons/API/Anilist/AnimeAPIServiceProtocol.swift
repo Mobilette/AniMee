a0d6e29a2e040c39830f66728760ce65dfa12f87
benaly <benaly.issouf@gmail.com>
2016-02-15 10:53:15 +0100

    Fetch anime episodes from server animee. Related to #2.

--- AniMee/Classes/Commons/API/Anilist/AnimeAPIServiceProtocol.swift
+++ AniMee/Classes/Commons/API/Anilist/AnimeAPIServiceProtocol.swift
@@ -12,6 +12,46 @@ import MobiletteFoundation
 
 protocol AnimeAPIServiceProtocol
 {
-    func authorize() -> Promise<MBOAuthCredential>
-    func fetchAnimeEpisodes() -> Promise<String>
+    static func fetchAnimeEpisodes() -> Promise<String>
+}
+
+extension AnimeAPIServiceProtocol {
+    private static func methodNotImplemented(functionName: String) -> Promise<String> {
+        return Promise<String> { fullfil, reject in
+            reject(WildAPIServiceProtocolError.NotImplemented(functionName).error)
+        }
+    }
+    static func fetchAnimeEpisodes() -> Promise<String> {
+        return self.methodNotImplemented("fetchAnimeEpisodes")
+    }
+}
+
+enum WildAPIServiceProtocolError: MBError
+{
+    case NotImplemented(String)
+    
+    var code: Int {
+        switch self {
+        case .NotImplemented:
+            return 1001
+        }
+    }
+    
+    var domain: String {
+        return "AnimeAPIServiceProtocolDomain"
+    }
+    
+    var description: String {
+        switch self {
+        case .NotImplemented:
+            return "API function is not implemented yet."
+        }
+    }
+    
+    var reason: String {
+        switch self {
+        case .NotImplemented(let functionName):
+            return "\(functionName) is not implemented yet."
+        }
+    }
 }
